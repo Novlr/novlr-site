8b53fac434702a78f1abc61db708fc62e6ef08c3
thomasmuirhead <mynovlr@gmail.com>
2014-01-18 16:16:23 +0000

    google analytics

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 app.set('view engine', 'jade')
 app.set('views', './templates')
 app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use(express.favicon(__dirname + '/favicon.ico')); 
 
